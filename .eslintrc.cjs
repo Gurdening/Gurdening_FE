3276ebe0a95fcfd56504eb539f774296f8eb2613
HarrySeop <parkpjs3083@naver.com>
2024-01-11 18:39:29 +0900

    🌱Feat : 초기 패키지 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React를 import하지 않아도 사용할 수 있게 함
+    parser: 'babel-parser', // babel parser를 사용하여, Parsing error를 해결함
+  },
+};
