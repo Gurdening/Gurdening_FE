69d5484eefb545d78469ccdf2422fbdcffc6dee1
김주하 <101048129+laketree2@users.noreply.github.com>
2024-01-30 16:19:08 +0900

    :lipstick: Category wrap 수정
    padding bottom 중복값 有

---
+++ src/pages/category/Category.Style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CategoryWrap = styled.div`
+  width: 100%;
+  padding: 10px;
+  padding-bottom: 50px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
