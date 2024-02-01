a681aeb87b119c10fbdbd3be53e28d4f11826293
HarrySeop <parkpjs3083@naver.com>
2024-02-02 05:05:19 +0900

    ✨Modify : 마이페이지 로그인 버튼 변경

--- src/components/myPageComp/LoginBtn.Style.jsx
+++ src/components/myPageComp/LoginBtn.Style.jsx
@@ -5,40 +5,36 @@ export const LoginWrapper = styled.div`
   align-items: center;
   justify-content: start;
   padding: 20px;
-  background-color: #fef5a6;
+  background-color: #fbf3c9;
   border-radius: 20px;
-  margin: 15px; // 래퍼 주위의 공간을 위한 마진
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); // 그림자 효과
+  margin: 40px 0;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  width: 100%;
 `;
 
 export const ProfilePicture = styled.img`
-  width: 50px;
-  height: 50px;
+  width: 70px;
+  height: 70px;
   border-radius: 50%;
-  margin-left: 5px;
   margin-right: 15px;
 `;
 
-export const StyledLoginBtn = styled.button`
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
+export const LoginTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
-  & .login-text {
-    display: block;
-    font-size: 14px;
-    color: #6b501e; // 글자 색상 설정
-    margin-bottom: 4px;
-  }
-
-  & .login-action {
-    display: block;
+  & .welcome-text {
     font-size: 18px;
-    font-weight: bold;
-    color: #473513; // 글자 색상 설정
+    font-weight: 500;
+    color: #6b501e;
+    margin-bottom: 13px;
   }
 
-  // &:hover {
-  //   opacity: 0.9;
-  // }
+  & .guide-text {
+    font-size: 18px;
+    color: #473513;
+    font-weight: 300;
+  }
 `;
