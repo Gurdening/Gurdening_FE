4b70ce9b730531884ad15214371d78d4162a1537
HarrySeop <parkpjs3083@naver.com>
2024-02-02 05:08:37 +0900

    💄Style : 페이지 스크롤 수정
    하단바에 가려지는 문제때문에 변경

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const ContentContainer = styled.div`
   background-color: #ffffff;
   overflow-y: scroll; // y축 스크롤 활성화
   overflow-x: hidden; // x축 스크롤 방지
-  height: calc(100vh - 50px); // NavBar 높이만큼 제외
+  height: calc(100vh - 90px); // NavBar 높이만큼 제외
   -webkit-overflow-scrolling: touch; // iOS 스크롤 부드럽게
 `;
 
