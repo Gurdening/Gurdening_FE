84a74bed5d50ddd9358189600e8f42d7e85be836
HarrySeop <parkpjs3083@naver.com>
2024-02-04 03:12:39 +0900

    🚧Modify: 카테고리 스크롤 문제 해결

--- src/pages/category/Category.Style.jsx
+++ src/pages/category/Category.Style.jsx
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const CategoryWrap = styled.div`
-  width: 100%;
   padding: 10px;
-  /* padding-bottom: 50px; */
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  overflow-y: auto;
+  height: 100%;
 `;
