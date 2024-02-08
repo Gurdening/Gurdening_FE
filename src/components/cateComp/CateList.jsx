7c7e981d0b5f8dae54890335c8851d55026130ef
HarrySeop <parkpjs3083@naver.com>
2024-02-09 05:17:32 +0900

    📝Add : 라우터 수정 + 추가

--- src/components/cateComp/CateList.jsx
+++ src/components/cateComp/CateList.jsx
@@ -5,14 +5,14 @@ import cookEmoji from '../../assets/emojis/emoji-cook.png';
 
 // 카테고리 데이터 리스트
 const categoryData = [
-  { name: '요리', icon: cookEmoji, path: '/menual1' },
-  { name: '일상생활', icon: cookEmoji, path: '/menual2' },
-  { name: '청결', icon: cookEmoji, path: '/menual3' },
-  { name: '치료', icon: cookEmoji, path: '/menual4' },
-  { name: '건강', icon: cookEmoji, path: '/menual5' },
-  { name: '가정', icon: cookEmoji, path: '/menual6' },
-  { name: '운동', icon: cookEmoji, path: '/menual7' },
-  { name: '법률', icon: cookEmoji, path: '/menual8' },
+  { name: '요리', icon: cookEmoji, path: '/category/COOKING' },
+  { name: '일상생활', icon: cookEmoji, path: '/category/DAILY_LIFE' },
+  { name: '청결', icon: cookEmoji, path: '/category/CLEANLINESS' },
+  { name: '치료', icon: cookEmoji, path: '/category/TREATMENT' },
+  { name: '건강', icon: cookEmoji, path: '/category/HEALTH' },
+  { name: '가정', icon: cookEmoji, path: '/category/HOME' },
+  { name: '운동', icon: cookEmoji, path: '/category/EXERCISE' },
+  { name: '법률', icon: cookEmoji, path: '/category/LAW' },
 ];
 
 const CategoryList = () => {
