5b66dcdb819faed4a87a1ad1bc1c0febaf0f4741
HarrySeop <parkpjs3083@naver.com>
2024-02-13 02:48:02 +0900

    🚚Modify : icon 최신화

--- src/components/cateComp/CateList.jsx
+++ src/components/cateComp/CateList.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import CateItem from './CateItem';
-import { CateListWrapper } from './CateList.Style';
-import cookEmoji from '../../assets/emojis/emoji-cook.png';
+import * as S from './CateList.Style';
+import Emoji_COOKING from '../../assets/emojis/emoji-cooking.png';
+import Emoji_DAILY_LIFE from '../../assets/emojis/emoji-dailylife.png';
+import Emoji_CLEANLINESS from '../../assets/emojis/emoji-cleanliness.png';
+import Emoji_TREATMENT from '../../assets/emojis/emoji-treatment.png';
+import Emoji_HEALTH from '../../assets/emojis/emoji-health.png';
+import Emoji_HOME from '../../assets/emojis/emoji-home.png';
+import Emoji_EXERCISE from '../../assets/emojis/emoji-exercise.png';
+import Emoji_LAW from '../../assets/emojis/emoji-law.png';
 
 // 카테고리 데이터 리스트
 const categoryData = [
-  { name: '요리', icon: cookEmoji, path: '/category/COOKING' },
-  { name: '일상생활', icon: cookEmoji, path: '/category/DAILY_LIFE' },
-  { name: '청결', icon: cookEmoji, path: '/category/CLEANLINESS' },
-  { name: '치료', icon: cookEmoji, path: '/category/TREATMENT' },
-  { name: '건강', icon: cookEmoji, path: '/category/HEALTH' },
-  { name: '가정', icon: cookEmoji, path: '/category/HOME' },
-  { name: '운동', icon: cookEmoji, path: '/category/EXERCISE' },
-  { name: '법률', icon: cookEmoji, path: '/category/LAW' },
+  { name: '요리', icon: Emoji_COOKING, path: '/category/COOKING' },
+  { name: '일상생활', icon: Emoji_DAILY_LIFE, path: '/category/DAILY_LIFE' },
+  { name: '청결', icon: Emoji_CLEANLINESS, path: '/category/CLEANLINESS' },
+  { name: '치료', icon: Emoji_TREATMENT, path: '/category/TREATMENT' },
+  { name: '건강', icon: Emoji_HEALTH, path: '/category/HEALTH' },
+  { name: '가정', icon: Emoji_HOME, path: '/category/HOME' },
+  { name: '운동', icon: Emoji_EXERCISE, path: '/category/EXERCISE' },
+  { name: '법률', icon: Emoji_LAW, path: '/category/LAW' },
 ];
 
 const CategoryList = () => {
   return (
-    <CateListWrapper>
+    <S.CateListWrapper>
       {categoryData.map((category) => (
         <CateItem
           key={category.name}
@@ -26,7 +33,7 @@ const CategoryList = () => {
           path={category.path}
         />
       ))}
-    </CateListWrapper>
+    </S.CateListWrapper>
   );
 };
 
