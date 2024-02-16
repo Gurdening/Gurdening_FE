e1f117045c15244a87c4ef8628d8c6c613c7b2c7
HarrySeop <parkpjs3083@naver.com>
2024-02-17 03:55:00 +0900

    ✨Add : 로고 클릭 핸들러 생성

--- src/components/logoComp/Logo.jsx
+++ src/components/logoComp/Logo.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import * as S from './Logo.Style';
+import { useNavigate } from 'react-router-dom';
 import LogoImg from '../../assets/images/BannerLogo.png';
 
 const Logo = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
   return (
     <S.LogoWrap>
-      <S.LogoImg src={LogoImg} alt="Gurdening " />
+      <S.LogoImg src={LogoImg} alt="Gurdening" onClick={handleLogoClick} />
     </S.LogoWrap>
   );
 };
